9abad7a15fabed849a33ca4802a1221ec8d78e28
khinthetnwektn <khinthet59@gmail.com>
2020-01-12 18:06:04 +0630

    11/01/2020

---
+++ loops.py
@@ -0,0 +1,139 @@
+#Loops
+
+-Whie Loops
+-for Loops
+
+Condition is true, while loop execute a set of statements
+
+x = 1
+while X < 7:
+	print(x)
+	x += 1
+
+
+
+While loop requires a variable ready.
+
+x = 1
+while x < 7:
+	print(x)
+	if x == 5:
+		break
+	x +=1
+
+==================================================
+
+For Loops
+
+#for loop is iterating over a sequence
+
+fruits = ["apple", "banana", "orange", "pineapple", "coconut", "cucumber"]
+for x in fruits:
+	print(x)
+
+#Looping in a String
+
+for x in "pineapple":
+	print(x)
+
+#The break Statement
+
+fruits = ["apple", "banana", "orange", "pineapple", "coconut", "cucumber"]
+for x in fruits:
+	print(x)
+	if x == "pineapple":
+		break
+
+
+-----
+
+fruits = ["apple", "banana", "orange", "pineapple", "coconut", "cucumber"]
+for x in fruits:
+	if x == "pineapple":
+		break
+	print(x)
+
+# The continue Statement - stop the current iteration
+
+fruits = ["apple", "banana", "orange", "pineapple", "coconut", "cucumber"]
+for x in fruits:
+	if x == "banana":
+		print(x)
+			continue
+		if x == "coconut":
+			continue
+	print(x)
+
+
+#The range() function - a set of code a specified number of times
+
+for x in range(10):
+	print(x)
+
+for x in range(10, 100):
+	print(x)
+
+for x in range(10, 100, 5):
+	print(x)
+
+
+#nested loops
+
+NumberA = [1, 2, 3, 4, 5]
+NumberB = [1, 2, 3, 4, 5]
+
+for x in NumberA:
+	for y in NumberB:
+		print(x,y)
+
+for x in range(1, 6):
+	for y in range(1, 6):
+		print(x, y)
+
+
+#Pass
+
+for x in [1, 2, 3, 4, 5]:
+	pass
+
+fruits = ["apple", "banana", "orange", "pineapple", "coconut", "cucumber"]
+for x in fruits:
+	if x == "pineapple":
+		pass
+	print(x)
+
+---------
+
+words = ['cat', 'window', 'defenstrate']
+for w in words:
+	print(w,  len(w))
+
+-------
+
+for n in range(2, 10):
+	for x in range(2, n):
+		if n % x == 0:
+			print(n, 'equals', x, '*', n//x)
+			break
+	else:
+		#loop fell through without finding a factor
+		print(n, 'is a prime number')
+
+------
+
+for num in range(2, 10):
+	if num % 2 == 0:
+		print("Found an even number", num)
+		continue
+	print("Found a number", num)
+
+-------
+
+>>> Fibonacci #module Folder
+
+def fib(n):
+	a, b = 0, 1
+	while a < n:
+		print(a, end=' ')
+		a, b = b, a+b
+	print()
