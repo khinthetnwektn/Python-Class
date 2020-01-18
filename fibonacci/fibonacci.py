26da200d71d411d58375f7dbe12349ae4cd0a862
khinthetnwektn <khinthet59@gmail.com>
2020-01-18 18:04:36 +0630

    add to weekly

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -4,14 +4,15 @@
 
 # n = int(input("Please enter a number : "))
 
-def fib(n):
+def fib(n):  #write Fibonacci series upto n
 	a, b = 0, 1
 	while a < n:
 		print(a)
 		a, b = b, a+b
 	print()
 
-fib(200)
+# fib(200)
+
 #Go to Fibonacci powerpoint
 
 def fib2(n): #return Fibonacci series upto n
@@ -22,8 +23,6 @@ def fib2(n): #return Fibonacci series upto n
 		a, b = b, a+b
 	return result
 
-fib2(200)
+# fib2(200)
 
 # >>> fib #import
-
-
